ca9e39348a6f1e20632af312bbc5fd79b5156cdf
Petteri Räty <betelgeuse@gentoo.org>
2009-04-30 00:10:04 +0300

    Add support for parsing first command line arg.
    If given a command line argument try to parse that, otherwise try to
    parse the hard coded string.

--- Main.java
+++ Main.java
@@ -1,20 +1,23 @@
 import org.antlr.runtime.*;
-import java.util.regex.*;
+//import java.util.regex.*;
 
 class Main {
     public static void main(String[] args) throws Exception {
-        DependLexer lex = new DependLexer(new ANTLRStringStream("foo-12"));
+        String source = args.length > 0 ? args[0] : "-3";
+        DependLexer lex = new DependLexer(new ANTLRStringStream(source));
+        /*
 		Pattern p = Pattern.compile(".*-\\d+$");
 		System.out.println(p.matcher("foo-123").matches());
-
+*/
        	CommonTokenStream tokens = new CommonTokenStream(lex);
 
         DependParser parser = new DependParser(tokens);
 
         try {
-            parser.pn();
+            parser.depend();
         } catch (RecognitionException e)  {
             e.printStackTrace();
+			System.exit(1);
         }
     }
 }
